91a68f8a5aefd7cd27fcd53da79dbdd6dab8cdac
wmatw <mathieu.wolf@gmail.com>
2018-11-06 12:32:55 +0200

    Sanity commit
    Added sln,
    Added x86 and x64 configuration
    Added LoadMe minimal project for easy test
    fix code compliance to rundll32
    warning level to 4, treat warnings as error

--- README.md
+++ README.md
@@ -4,4 +4,28 @@ Small utility to invoke managed DLL function via rundll32
 With this utility you can run any managed dll functions without the need of altering the managed code.
 The synatx is:
 
-rundll32.exe managed.dll,Run [Managed Dll Path],[Managed Function]
+64bit
+rundll32.exe managed64.dll,Run [Managed Dll Path],[Managed Function]
+32bit
+rundll32.exe managed32.dll,Run [Managed Dll Path],[Managed Function]
+
+After building the solution for both platform (x86 and x64), you can test the basic functionality with:
+
+for a 64 bit computer:
+
+cd <Release Folder> 
+rundll32 managed64.dll,Run LoadMe.dll,HelloWorld1
+rundll32 managed64.dll,Run LoadMe.dll,HelloWorld2
+%windir%\SysWOW64\rundll32 managed32.dll,Run LoadMe.dll,HelloWorld1
+%windir%\SysWOW64\rundll32 managed32.dll,Run LoadMe.dll,HelloWorld2
+
+for a 32 bit computer:
+
+cd <Release Folder> 
+rundll32 managed32.dll,Run LoadMe.dll,HelloWorld1
+rundll32 managed32.dll,Run LoadMe.dll,HelloWorld2
+
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -5,23 +5,35 @@ using namespace System;
 using namespace System::Reflection;
 using namespace std;
 
+
+//The entry point function for Rundll32 must be defined with the _stdcall calling convention (CALLBACK defaults to using the _stdcall attribute). 
+//If the _stdcall attribute is missing, then the function defaults to _cdecl calling convention
+//and then Rundll32 will terminate abnormally after calling the function.
+//Since we must declare the function with _stdcall calling convention as described above,
+//it follows that the Visual C++ compiler will actually export it as _Runt@16 for 32 bit build, for 64 bit it is done automatically by the linker
+
+#ifndef _WIN64
+#pragma comment(linker, "/EXPORT:Run=_Run@16")
+#endif 
+
+
 extern "C"
 {
-	__declspec(dllexport) BOOL WINAPIV Run(HWND hwnd, HINSTANCE hinst, LPSTR pszCmdLine, int nCmdShow)
+	__declspec(dllexport) BOOL CALLBACK Run(HWND /*hwnd*/, HINSTANCE /*hinst*/, LPSTR pszCmdLine, int /*nCmdShow*/)
 	{
 		String ^cmdLine = gcnew String(pszCmdLine);
 		cli::array<String^>^ inputValues = cmdLine->Split(',');
-		Assembly ^ dllHolder = Assembly::LoadFile(inputValues[0]);
+		Assembly ^ dllHolder = Assembly::LoadFrom(inputValues[0]); //as rundll32 exact path is not required
 		cli::array<Type^> ^allTypes = dllHolder->GetExportedTypes();
 
-		for (int i = 0; i < allTypes->Length; i++)
+		for each (auto type in allTypes)
 		{
 			try
 			{
-				Console::WriteLine("Looking for method {0} in Class {1}", inputValues[1], allTypes[i]->FullName);
-				allTypes[i]->InvokeMember(inputValues[1], BindingFlags::InvokeMethod, nullptr,
-					Activator::CreateInstance(allTypes[i]), gcnew cli::array<Object^>{});
-				Console::WriteLine("Found It! Finished running method {0} in Class {1}", inputValues[1], allTypes[i]->FullName);
+				Console::WriteLine("Looking for method {0} in Class {1}", inputValues[1], type->FullName);
+				type->InvokeMember(inputValues[1], BindingFlags::InvokeMethod, nullptr,
+					Activator::CreateInstance(type), gcnew cli::array<Object^>{});
+				Console::WriteLine("Found It! Finished running method {0} in Class {1}", inputValues[1], type->FullName);
 				return TRUE;
 
 			}
@@ -36,7 +48,9 @@ extern "C"
 	}
 }
 
-BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+#pragma unmanaged
+
+BOOL APIENTRY DllMain(HINSTANCE /*hinstDLL*/, DWORD fdwReason, LPVOID /*lpReserved*/)
 {
 	switch (fdwReason)
 	{
@@ -51,8 +65,10 @@ BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 }
 
 /* For Testing - Change to Debug*/
-int main(int argc, TCHAR* argv[])
+int main(int argc, CHAR* argv[])
 {
+	if (!argc)
+		return -1;
 	Run(NULL, NULL, argv[1], NULL);
 	return 0;
 }
